da518aa170c496af49e7ba29d8837123c16f9856
AndyShan <sgd1995@163.com>
2016-09-04 14:59:20 +0800

    add a better way

--- _151/Solution.java
+++ _151/Solution.java
@@ -2,23 +2,14 @@ package _151;
 
 public class Solution {
     public String reverseWords(String s) {
-    	String m_s = s.trim();
-    	char[] array = (" " + m_s).toCharArray();
-    	int length = array.length;
-    	String ss = "";
-    	String sss = "";
-    	for (int i = length - 1;i >= 0;i--) {
-    		if (array[i] != ' ') {
-    			sss += array[i];
-    		}else if (!sss.equals("")) {
-    			ss += new StringBuffer(sss).reverse().toString() + " ";
-    			sss = "";
-    		}
-    	}
-    	return ss.trim();
+        String[] s1 = s.split("\\s");
+        StringBuilder sb = new StringBuilder("");
+        for (int i = s1.length-1; i >= 0; i--) {
+			if (!s1[i].trim().equals("")) {
+    			sb.append(s1[i]);
+	    		sb.append(' ');
+			}
+		}
+        return sb.toString().trim();
     }
-//    public static void main(String args[]) {
-//    	String s = "1    5 3";
-//    	System.out.println(new Solution().reverseWords(s));
-//    }
 }
